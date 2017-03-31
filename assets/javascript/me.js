0bd076f70269fb06906ec2e2f42236e24dbff38b
Pauline Whang <paulinewhang@Paulines-MacBook-Pro.local>
2017-03-31 00:31:04 -0400

    second push

---
+++ assets/javascript/me.js
@@ -0,0 +1,11 @@
+var tags = [
+ 'greens',
+ 'smoothie',
+ 'sushi',
+ 'salads',
+];
+$(document).ready(function() {
+	$('.giphy-tag-wrapper button').on('click', function(){
+		console.log('pieface');
+	});
+});
